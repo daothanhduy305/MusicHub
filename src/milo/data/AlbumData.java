e0112399205e891bedd5cb5461c881d31f46f72d
Ebolo <daothanhduy305@gmail.com>
2016-12-06 22:21:36 +0100

    Add the implementation for Class AlbumData
    Signed-off-by: Ebolo <daothanhduy305@gmail.com>

--- src/milo/data/AlbumData.java
+++ src/milo/data/AlbumData.java
@@ -1,11 +1,47 @@
 package milo.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class name:  AlbumData
  * Description: This class is the data-type that holds all info about a specific album
  */
 
-public class AlbumData implements Serializable {
+public class AlbumData implements Serializable, Comparable<AlbumData> {
+    private String mTitle, mAuthor;
+    private List<SongData> mSongList;
+    private byte[] albumArtByte;
+
+    public AlbumData(String albumTitle, String albumAuthor) {
+        this.mAuthor = albumAuthor;
+        this.mTitle = albumTitle;
+        mSongList = new ArrayList<>(20);
+    }
+
+    public String getAlbumAuthor() {
+        return mAuthor;
+    }
+
+    public String getAlbumTitle() {
+        return mTitle;
+    }
+
+    public List<SongData> getSongList() {
+        return mSongList;
+    }
+
+    public byte[] getAlbumArtByte() {
+        return albumArtByte;
+    }
+
+    public void setAlbumArtByte(byte[] albumArtByte) {
+        this.albumArtByte = albumArtByte;
+    }
+
+    @Override
+    public int compareTo(AlbumData o) {
+        return (this.mTitle.compareToIgnoreCase(o.mTitle));
+    }
 }
