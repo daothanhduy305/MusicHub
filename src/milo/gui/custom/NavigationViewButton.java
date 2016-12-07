ab5f0c9190aadd166b95ee53456ce13351dcc8c8
amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>
2016-12-07 11:16:59 +0100

    Add Class for navigation button

---
+++ src/milo/gui/custom/NavigationViewButton.java
@@ -0,0 +1,11 @@
+package milo.gui.custom;
+
+import javafx.scene.layout.HBox;
+
+/**
+ * Class name:  NavigationViewButton
+ * Description: This is the class for the custom buttons, which will be used as navigation buttons to change views
+ */
+
+public class NavigationViewButton extends HBox {
+}
