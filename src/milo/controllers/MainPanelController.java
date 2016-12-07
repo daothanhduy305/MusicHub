87afaba5656666cab351c2d4f91804a06b38b903
amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>
2016-12-07 11:56:44 +0100

    Add design templates for views Add new view: mainPanel and its controller
    Signed-off-by: amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>

---
+++ src/milo/controllers/MainPanelController.java
@@ -0,0 +1,20 @@
+package milo.controllers;
+
+import milo.controllers.abstractcontrollers.AbstractSubUIController;
+
+/**
+ * Class name:  MainPanelController
+ * Description: This is the controllers for the main panel, which consists of all the views (all songs view, albums,...
+ */
+
+public class MainPanelController extends AbstractSubUIController {
+    @Override
+    public void buildUI() {
+
+    }
+
+    @Override
+    public void refreshUI() {
+
+    }
+}
