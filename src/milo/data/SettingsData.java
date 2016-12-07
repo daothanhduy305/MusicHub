c4efc34d959e5c00e6134f02b2da0eb5d49ebe97
amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>
2016-12-07 11:22:26 +0100

    Add Class for settings data
    Signed-off-by: amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>

---
+++ src/milo/data/SettingsData.java
@@ -0,0 +1,11 @@
+package milo.data;
+
+import java.io.Serializable;
+
+/**
+ * Class name:  SettingsData
+ * Description: This is the data-type for settings
+ */
+
+public class SettingsData implements Serializable {
+}
