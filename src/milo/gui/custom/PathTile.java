a35712d8964f795c5df62cdbb7d7fee8f448356e
Ebolo <daothanhduy305@gmail.com>
2016-12-19 16:48:32 +0100

    Implement PathTile as custom UI element
    Signed-off-by: Ebolo <daothanhduy305@gmail.com>

--- src/milo/gui/custom/PathTile.java
+++ src/milo/gui/custom/PathTile.java
@@ -1,22 +1,33 @@
 package milo.gui.custom;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import milo.gui.utils.Constants;
+
+import java.io.IOException;
 
 /**
  * Created by Ebolo on 19/12/2016.
  */
-public class PathTile extends Pane {
-    private VBox infoBox;
-    private Label folderName, folderPath;
+public class PathTile extends VBox {
+    @FXML private Label folderName, folderPath;
 
     public PathTile(String fPath) {
-        folderPath = new Label(fPath);
-        folderName = new Label(fPath);
-        infoBox = new VBox(folderName, folderPath);
-        this.getChildren().add(infoBox);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/milo/gui/designs/path_tile.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        folderName.setText(fPath);
+        folderPath.setText(fPath);
 
         this.setBackground(Constants.getBgButBlue());
         this.setOnMouseEntered(event -> setBackground(Constants.getBgButGray()));
